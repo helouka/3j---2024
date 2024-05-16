234c2f2c33b9b1acf309587468f67ac2b4448780
Heloísa <106184298+helouka@users.noreply.github.com>
2024-05-16 19:33:16 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelector(".botao")
+console. log(botoes)
+for (let i = 0; i < botoes.length ; i++){
+  botoes[i]. onclik = function(){
+    for(j=0; j<botoes.length;j++){
+    botoes[j].classList.remove("ativo")
+    }
+    botoes[j].classList.add("ativo")
+    }
+}
+
